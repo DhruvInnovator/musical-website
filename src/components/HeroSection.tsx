7977c5d9f946c6e5ddd6eaa65b3a2611a6e727e0
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-04-20 09:05:54 +0530

    little changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ export default function herosection() {
         fill="purple"
       />
             <div className='p-4 relative z-10 w-full text-center'>
-                <h1 className='text-white mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Mlaster The Art Of Music</h1>
+                <h1 className='text-white mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master The Art Of Music</h1>
                 <p className='mt-8 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className='mt-8'>
                 <Link href="/" className='text-white'>
--- src/components/ui/FeaturedCourses.tsx
+++ src/components/ui/FeaturedCourses.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 function FeaturedCourses() {
   return (
-    <div className="py-12 bg-gray-900">
+    <div className="py-64 bg-gray-900">
         <div className="text-center">
             <h2 className="text-base text-purple-600 font-semibold tracking-wide uppercase">COURSE KNOWLEDGE</h2>
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-purple-400 sm:text-4xl">Learning the best</p>
@@ -11,7 +11,7 @@ function FeaturedCourses() {
         <div></div>
         <div className="mt-20 text-center">
         <Link href={"/courses"}
-            className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+            className="px-6 py-6 rounded-full border border-neutral-600 text-white bg-purple-800 hover:bg-black  transition duration-200"
             >
             View All courses
         </Link>
