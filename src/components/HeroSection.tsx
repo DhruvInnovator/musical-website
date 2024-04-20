9234cf6d92a50ecd62b521edc9a299c18c1baa17
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-04-20 09:05:28 +0530

    button change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,9 +12,9 @@ export default function herosection() {
         fill="purple"
       />
             <div className='p-4 relative z-10 w-full text-center'>
-                <h1 className='text-white mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Blaster The Art Of Music</h1>
-                <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.</p>
-            <div className='mt-4'>
+                <h1 className='text-white mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Mlaster The Art Of Music</h1>
+                <p className='mt-8 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+            <div className='mt-8'>
                 <Link href="/" className='text-white'>
                 <Button borderRadius='2rem' className='bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800 drop-shadow-lg'>
                     Explore Courses
