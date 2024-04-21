854524b261ea20f6c55f91bf5faee30a47ae453e
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-04-21 13:15:10 +0530

    featured courses added with card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/ui/FeaturedCourses";
 
+
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.88]">
+    
     <HeroSection />
     <FeaturedCourses />
+  
+    
   </main>
   
   );
