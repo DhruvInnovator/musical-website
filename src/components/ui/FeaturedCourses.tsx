bfc0ee71cf095c7fd8ca2b0d39c0eeca54732ff8
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-04-22 15:17:30 +0530

    enhancement

--- src/components/ui/FeaturedCourses.tsx
+++ src/components/ui/FeaturedCourses.tsx
@@ -4,6 +4,7 @@ import courseData from "@/data/music_courses.json"
 import Link from "next/link"
 import { BackgroundGradient } from "./background-gradient"
 import { Button } from "./moving-border"
+import { HoverBorderGradient } from "./hover-border-gradient"
 
 // import { createContext } from 'react';
 
@@ -24,7 +25,7 @@ function FeaturedCourses() {
 
 
   return (
-    <div className="py-64 w-screen px-64 bg-gray-900">
+    <div className="py-56 w-screen px-56 bg-gray-900">
         <div >
           <div className="text-center">
             <h2 className="text-base text-purple-600 font-semibold tracking-wide uppercase">COURSE KNOWLEDGE</h2>
@@ -38,19 +39,19 @@ function FeaturedCourses() {
             {featuredCourses.map((course:Course) => (
               <div key={course.id} className="flex justify-center">
                 <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
-                  <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                    <li>
-                      <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                  <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow ">
+                    <li className="min-h-[80%]">
+                      <p className="text-9xl sm:text-xl text-black mt-2 mb-4 dark:text-neutral-200">{course.title}</p>
                       <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
                       <p className="text-purple-500">Price: ${course.price}</p>
                       <p className="text-purple-600">Instructor: {course.instructor}</p>
                     </li>
                     
-                    <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-2xl mt-8">
-                    <Link href={`/course/${course.slug}`} className="text-purple-700 ">
+                    <Link href={`/course/${course.slug}`} className="text-purple-700">
+                    <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold  py-2 px-4 rounded-2xl mt-5 text-sm">
                       Learn More
-                    </Link>
                     </button>
+                    </Link>
                     
                   </div>
                 </BackgroundGradient>
@@ -58,12 +59,15 @@ function FeaturedCourses() {
             ))}
           </div>
         </div>        
-        <div className="mt-20 text-center">
-        <Link href={"/courses"}
-            className="px-4 py-4 rounded-full border text-md border-neutral-600 text-white bg-purple-800 hover:bg-black  transition duration-200"
-            >
+        <div className="mt-20 w-full flex justify-center items-center">
+          <Link href={"/courses"}>
+          <HoverBorderGradient>
             View All courses
-        </Link>
+          </HoverBorderGradient>
+          </Link>
+        
+
+       
         </div>
 
     </div>
