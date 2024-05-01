99234a832f206ef81f65e2e4155e2ca8abd7d362
DhRuVUser04 <156186518+DhRuVUser04@users.noreply.github.com>
2024-05-01 15:48:27 +0530

    Update DataMovingCards.tsx

--- src/components/DataMovingCards.tsx
+++ src/components/DataMovingCards.tsx
@@ -51,7 +51,7 @@ function InfiniteMovingCardsDemo() {
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
         items = {DataMovingCard}
-        direction = "left"
+        direction = "right"
         speed="slow"
       />
     </div>
